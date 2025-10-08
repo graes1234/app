a5759fb6cbbae4a07eb42c39623f8abee9b1be43
graes1234 <117164702+graes1234@users.noreply.github.com>
2025-10-08 13:33:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -115,8 +115,8 @@ $cameraBtn.addEventListener("click", async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: {
         facingMode: { ideal: "environment" },
-        width: { ideal: 1280 },   // 원하는 해상도
-        height: { ideal: 720 },   // 세로 스트림
+        //width: { ideal: 1280 },   // 원하는 해상도
+        //height: { ideal: 720 },   // 세로 스트림
         //aspectRatio: 16 / 9 },    //가로 세로 비율
       //video: { facingMode: { ideal: "environment" } 
         },
