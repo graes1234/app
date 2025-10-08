4708bf9723e33e869e4889f00a848784c2f2b18a
graes1234 <117164702+graes1234@users.noreply.github.com>
2025-10-08 13:31:40 +0900

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ $cameraBtn.addEventListener("click", async () => {
         facingMode: { ideal: "environment" },
         width: { ideal: 1280 },   // 원하는 해상도
         height: { ideal: 720 },   // 세로 스트림
-        aspectRatio: 16 / 9 },    //가로 세로 비율
+        //aspectRatio: 16 / 9 },    //가로 세로 비율
       //video: { facingMode: { ideal: "environment" } 
         },
       audio: false
